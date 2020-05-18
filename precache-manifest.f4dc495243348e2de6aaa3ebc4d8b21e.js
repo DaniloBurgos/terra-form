868a145f0ccb979ece8b573f7ab5b05cdc929e78
Danilo Burgos <ilianburgos@gmail.com>
2020-05-18 07:19:22 -0500

    Updates

---
+++ precache-manifest.f4dc495243348e2de6aaa3ebc4d8b21e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7daf9c7b3e1c2f68d1d0b58741a4a7cd",
+    "url": "/terra-form/index.html"
+  },
+  {
+    "revision": "cde2c609aee545729702",
+    "url": "/terra-form/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "82bfe207082922c0dac0",
+    "url": "/terra-form/static/js/2.e6e54068.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/terra-form/static/js/2.e6e54068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cde2c609aee545729702",
+    "url": "/terra-form/static/js/main.f96efa47.chunk.js"
+  },
+  {
+    "revision": "036ed4e271c26edb533e",
+    "url": "/terra-form/static/js/runtime-main.cad5ea78.js"
+  }
+]);
